f96026e23ff50396b04aa3edd5381fe2229505b4
sally0226 <sally734595@gmail.com>
2021-05-14 14:22:12 +0900

    styled-component 사용 안하게 변경
    material UI TextField 사용시에, styled-component 사용해서 디자인 변경했던 것을 scss 사용하도록 변경

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -4,24 +4,6 @@ import { TextField, Button, Box, Grid } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
 import { useLoginUser } from '../MVVM/ViewModel/UserViewModel';
 
-const CssTextField = withStyles({
-	root: {
-		'& label.Mui-focused': {
-			color: '#464E47',
-		},
-		'& .MuiOutlinedInput-root': {
-			'& fieldset': {
-				borderColor: '#464E47',
-			},
-			'&:hover fieldset': {
-				borderColor: '#96E6B3',
-			},
-			'&.Mui-focused fieldset': {
-				borderColor: '#96E6B3',
-			},
-		},
-	},
-})(TextField);
 function LogIn({ history }) {
 	const [id, setId] = useState();
 	const [pw, setPw] = useState();
@@ -46,10 +28,10 @@ function LogIn({ history }) {
 			<div className="form">
 				<Grid>
 					<Box mt={2}>
-						<CssTextField variant="outlined" label="ID" placeholder="아이디를 입력하세요" value={id} onChange={idHandler} />
+						<TextField variant="outlined" label="ID" placeholder="아이디를 입력하세요" value={id} onChange={idHandler} />
 					</Box>
 					<Box mt={2}>
-						<CssTextField variant="outlined" label="PW" type="password" placeholder="비밀번호를 입력하세요" value={pw} onChange={pwHandler} />
+						<TextField variant="outlined" label="PW" type="password" placeholder="비밀번호를 입력하세요" value={pw} onChange={pwHandler} />
 					</Box>
 				</Grid>
 				<Button className="loginBtn" variant="contained" onClick={loginHandler}>LogIn</Button>
